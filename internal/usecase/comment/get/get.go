c916c743d3f004e53af38bdb0ca0424dbb8329d6
ngorodnyansky <ngorodnyansky@burut.org>
2025-06-25 16:49:11 +0300

    Правки нейминга

--- internal/usecase/comment/get/get.go
+++ internal/usecase/comment/get/get.go
@@ -7,11 +7,11 @@ import (
 	"github.com/evrone/go-clean-template/internal/entity"
 )
 
-// History - getting all comments history from store fore entity.
+// GetComments - getting all comments history from store fore entity.
 func (uc *UseCase) GetComments(ctx context.Context, e entity.Entity) (entity.CommentsList, error) {
 	comments, err := uc.repo.GetComments(ctx, e)
 	if err != nil {
-		return entity.CommentsList{}, fmt.Errorf("CommentsUseCase - History - s.repo.GetComments: %w", err)
+		return entity.CommentsList{}, fmt.Errorf("CommentsUseCase - GetComments - uc.repo.GetComments: %w", err)
 	}
 
 	return entity.CommentsList{EntityID: e.EntityID, EntityType: e.EntityType, Comments: comments}, nil
