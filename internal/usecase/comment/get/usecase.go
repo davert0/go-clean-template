025f7accef2ed081250e181524c417416a6750a8
ngorodnyansky <gorodnyansky.n@gmail.com>
2025-06-27 00:00:19 +0300

    Исправил ошибки  после merge

--- internal/usecase/comment/get/usecase.go
+++ internal/usecase/comment/get/usecase.go
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:internal/usecase/comment/get/usecase.go
 package get
 
 import (
@@ -16,22 +15,3 @@ func New(r repo.CommentsRepo) *UseCase {
 		repo: r,
 	}
 }
-========
-package create
-
-import (
-	"github.com/evrone/go-clean-template/internal/repo"
-)
-
-// UseCase -.
-type UseCase struct {
-	repo repo.CommentsRepo
-}
-
-// New -.
-func New(r repo.CommentsRepo) *UseCase {
-	return &UseCase{
-		repo: r,
-	}
-}
->>>>>>>> remotes/origin/comment_handle:internal/usecase/comment/create/usecase.go
